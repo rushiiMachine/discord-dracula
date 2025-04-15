e7f0123ee78a1caa020a20ebc8e95b552b96c927
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-04-15 11:22:25 -0700

    feat: setup build tools

---
+++ scripts/dev.js
@@ -0,0 +1,82 @@
+import {copyFileSync, existsSync, writeFileSync} from "node:fs";
+import {join} from "node:path";
+import {watch} from "chokidar";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import * as sass from "sass";
+
+const dotenvResult = dotenv.config();
+const themesDir = dotenvResult.parsed?.THEMES_DIR;
+
+const srcDir = join(import.meta.dirname, "../src");
+const srcPath = join(import.meta.dirname, "../src/main.scss");
+const devCssPath = join(import.meta.dirname, "../dist/dev.css");
+
+if (dotenvResult.error || !themesDir) {
+	const envPath = join(import.meta.dirname, "../.env");
+	const envExamplePath = join(import.meta.dirname, "../.env.example");
+
+	console.log(chalk.red("In order to test this theme, set the "
+		+ chalk.underline("THEMES_DIR") + " variable in .env to your client mod's "
+		+ chalk.underline("themes") + " directory!"));
+	console.log();
+
+	if (!existsSync(envPath)) {
+		copyFileSync(envExamplePath, envPath);
+	}
+
+	process.exit(1);
+}
+
+if (!existsSync(themesDir)) {
+	console.log(chalk.red("Specified theme directory "
+		+ chalk.underline("THEMES_DIR") + " in .env does not exist! "
+		+ chalk.white("(")
+		+ chalk.gray(chalk.dim(themesDir))
+		+ chalk.white(")")));
+	console.log();
+
+	process.exit(1);
+}
+
+function dateHeader() {
+	const date = new Date().toISOString()
+		.replace(/T/, ' ')
+		.replace(/\..+/, '');
+
+	return chalk.gray(`[${date}]`);
+}
+
+function compile() {
+	const remoteCssPath = join(themesDir, "dracula.theme.css");
+
+	try {
+		const result = sass.compile(srcPath, {
+				sourceMap: false,
+				style: "expanded",
+				verbose: true,
+			}
+		);
+
+		writeFileSync(devCssPath, result.css);
+		writeFileSync(remoteCssPath, result.css);
+	} catch (e) {
+		console.log(dateHeader() + chalk.red(" Failed to compile theme"));
+		console.log(e.sassMessage ? e.message : e);
+	}
+}
+
+console.log(chalk.dim("Writing to theme directory " + chalk.gray(chalk.dim(themesDir))));
+console.log();
+
+compile();
+console.log(dateHeader() + chalk.green(" Successfully compiled theme."));
+
+if (process.argv.includes("--watch")) {
+	console.log(dateHeader() + chalk.green(" Watching for changes..."));
+
+	watch(srcDir).on("change", () => {
+		compile();
+		console.log(dateHeader() + chalk.green(" Successfully recompiled theme."));
+	});
+}
---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+// noinspection JSUnusedGlobalSymbols
+
+/** @type {import('stylelint').Config} */
+export default {
+	extends: ["stylelint-config-standard-scss"],
+	rules: {},
+};
